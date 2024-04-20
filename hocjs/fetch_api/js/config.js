15dcf51d5ef2dd3547be26eb00a1c5e95a300d1e
Hoàng An F8 <hoangan@fullstack.edu.vn>
2024-04-20 12:49:10 +0700

    Update code

---
+++ hocjs/fetch_api/js/config.js
@@ -0,0 +1,5 @@
+const config = {
+  serverApi: `https://api.escuelajs.co/api/v1`,
+};
+
+export default config;
--- hocjs/fetch_api/js/ex02.js
+++ hocjs/fetch_api/js/ex02.js
@@ -27,8 +27,13 @@
 
 // sessionStorage.setItem("token", "123");
 
+import config from "./config.js";
+import { httpClient } from "./client.js";
+const { serverApi } = config;
+httpClient.serverApi = serverApi; //Cập nhật serverApi cho request
+
 const root = document.querySelector("#root");
-const serverApi = `https://api.escuelajs.co/api/v1`;
+
 const app = {
   loginForm: function () {
     return `<form action="" class="login-form">
@@ -81,45 +86,66 @@ const app = {
   sendRequestLogin: async function (data) {
     //gọi api login
 
-    const response = await fetch(`${serverApi}/auth/login`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    });
+    const { res: response, data: tokens } = await httpClient.post(
+      "/auth/login",
+      data
+    );
+
     //Thất bại --> Thông báo lỗi
     if (!response.ok) {
       alert("Email hoặc mật khẩu không chính xác");
       return;
     }
     //Nếu thành công --> Lưu vào localStorage
-    const tokens = await response.json();
     localStorage.setItem("tokens", JSON.stringify(tokens));
     this.render();
   },
+  sendRequestRefreshToken: async function (refreshToken) {
+    try {
+      const { res: response, data } = await httpClient.post(
+        "/auth/refresh-token",
+        { refreshToken }
+      );
+
+      if (!response.ok) {
+        throw new Error("Refresh Token Invalid");
+      }
+      return data;
+    } catch {
+      return false;
+    }
+  },
   sendRequestProfile: async function () {
-    const { access_token: accessToken } = JSON.parse(
-      localStorage.getItem("tokens")
-    );
-    const response = await fetch(`${serverApi}/auth/profile`, {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-    });
+    const { access_token: accessToken, refresh_token: refreshToken } =
+      JSON.parse(localStorage.getItem("tokens"));
+
+    httpClient.token = accessToken;
+
+    const { res: response, data } = await httpClient.get("/auth/profile");
     //Kiểm tra:
     //Nếu thất bại --> Đăng xuất
     if (!response.ok) {
       //Gọi API refresh-token để cấp lại access token mới
       //Lưu access token vào localStorage
       //Gọi lại request profile
-
-      localStorage.removeItem("tokens");
-      this.render();
+      // localStorage.removeItem("tokens");
+      // this.render();
+      //Gọi API Refresh token
+      const newToken = await this.sendRequestRefreshToken(refreshToken);
+      if (!newToken) {
+        //--> Nếu lỗi --> Đăng xuất
+        localStorage.removeItem("tokens");
+        this.render();
+      } else {
+        //Nếu thành công
+        // - Lưu token mới vào Storage
+        localStorage.setItem("tokens", JSON.stringify(newToken));
+        // - Gọi lại request bị lỗi
+        this.sendRequestProfile();
+      }
+      return;
     }
     //Nếu thành công --> Lấy dữ liệu và hiển thị lên trình duyệt
-    const data = await response.json();
     const profileNameEl = root.querySelector(".profile-name");
     profileNameEl.innerText = data.name;
   },
@@ -156,69 +182,74 @@ Ví dụ:
 - Request 3 --> Success theo access token mới
 */
 
-let accessToken = "ahihi";
-let isExpired = false;
-let isRefreshToken = false;
-let refreshTokenPromise = null;
-const refreshToken = () => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      const token = Math.random();
-      console.log(token);
-      resolve("new token: " + token);
-    }, 1000);
-  });
-};
-const requestApi = (url) => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      //Giả sử request /courses bị hết hạn token
-      if (!isRefreshToken && url === "/posts") {
-        isExpired = true;
-      }
-      if (isExpired) {
-        reject("Token expired");
-      }
-      resolve(`Data by Request ${url} with Token: ${accessToken}`);
-    }, 1000);
-  });
-};
+// let accessToken = "ahihi";
+// let isExpired = false;
+// let isRefreshToken = false;
+// let refreshTokenPromise = null;
+// const refreshToken = () => {
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       const token = Math.random();
+//       console.log(token);
+//       resolve("new token: " + token);
+//     }, 1000);
+//   });
+// };
+// const requestApi = (url) => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       //Giả sử request /courses bị hết hạn token
+//       if (!isRefreshToken && url === "/posts") {
+//         isExpired = true;
+//       }
+//       if (isExpired) {
+//         reject("Token expired");
+//       }
+//       resolve(`Data by Request ${url} with Token: ${accessToken}`);
+//     }, 1000);
+//   });
+// };
 
 //Viết 1 hàm mới để xử lý gọi API và cấp lại access token khi bị hết hạn
-const httpClient = async (url) => {
-  try {
-    const response = await requestApi(url);
-    return response;
-  } catch (e) {
-    //Khi request bị lỗi --> Gọi tới api refresh token
-    if (!refreshTokenPromise) {
-      refreshTokenPromise = refreshToken();
-    }
-    const newAccessToken = await refreshTokenPromise;
-    accessToken = newAccessToken;
-    isExpired = false;
-    isRefreshToken = true;
-    const response = await requestApi(url);
-    return response;
-  }
-};
+// const httpClient = async (url) => {
+//   try {
+//     const response = await requestApi(url);
+//     return response;
+//   } catch (e) {
+//     //Khi request bị lỗi --> Gọi tới api refresh token
+//     if (!refreshTokenPromise) {
+//       refreshTokenPromise = refreshToken();
+//     }
+//     const newAccessToken = await refreshTokenPromise;
+//     accessToken = newAccessToken;
+//     isExpired = false;
+//     isRefreshToken = true;
+//     const response = await requestApi(url);
+//     return response;
+//   }
+// };
 
-//Call API
-httpClient("/profile").then((data) => {
-  console.log(data);
-});
+// //Call API
+// httpClient("/profile").then((data) => {
+//   console.log(data);
+// });
 
-//Expire --> Cấp lại access mới
-httpClient("/courses").then((data) => {
-  console.log(data);
-});
+// //Expire --> Cấp lại access mới
+// httpClient("/courses").then((data) => {
+//   console.log(data);
+// });
 
-//Expire --> Chờ
-httpClient("/posts").then((data) => {
-  console.log(data);
-});
+// //Expire --> Chờ
+// httpClient("/posts").then((data) => {
+//   console.log(data);
+// });
 
-//Expire --> Chờ
-httpClient("/my-course").then((data) => {
-  console.log(data);
-});
+// //Expire --> Chờ
+// httpClient("/my-course").then((data) => {
+//   console.log(data);
+// });
+
+//CORS
+//localStorage
+//sessionStorage
+//cookie, session
