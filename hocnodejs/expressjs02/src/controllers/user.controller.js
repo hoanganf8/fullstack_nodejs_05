ea0c15e7d7a183e7b79a24e706d489a754ebef44
Hoàng An F8 <hoangan@fullstack.edu.vn>
2024-07-30 10:08:02 +0700

    update code k5 day68

--- hocnodejs/expressjs02/src/controllers/user.controller.js
+++ hocnodejs/expressjs02/src/controllers/user.controller.js
@@ -1,7 +1,27 @@
-const { User } = require("../models/index");
+const { User, Phone } = require("../models/index");
 module.exports = {
   index: async (req, res) => {
     const users = await User.findAll();
     res.json({ users });
+  },
+  find: async (req, res) => {
+    const { id } = req.params;
+    const user = await User.findByPk(id);
+    const phone = await user.getPhone();
+    res.json({ user, phone: phone.value });
+  },
+  findByPhone: async (req, res) => {
+    const { phone } = req.params;
+    //Trả về instance của phone
+    const phoneInstance = await Phone.findOne({
+      where: {
+        value: phone,
+      },
+    });
+    if (!phoneInstance) {
+      return res.json({ error: "Không tìm thấy" });
+    }
+    const user = await phoneInstance.getUser();
+    res.json({ phone, user });
   },
 };
--- hocnodejs/expressjs02/src/models/phone.js
+++ hocnodejs/expressjs02/src/models/phone.js
@@ -1,7 +1,7 @@
 "use strict";
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
+  class Phone extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -9,31 +9,29 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      User.hasOne(models.Phone, {
+      Phone.belongsTo(models.User, {
         foreignKey: "user_id",
-        as: "phone",
+        as: "user",
       });
     }
   }
-  User.init(
+  Phone.init(
     {
       id: {
         type: DataTypes.INTEGER,
         primaryKey: true,
         autoIncrement: true,
       },
-      fullname: DataTypes.STRING,
-      email: DataTypes.STRING,
-      password: DataTypes.STRING,
-      status: DataTypes.BOOLEAN,
+      value: DataTypes.STRING,
+      user_id: DataTypes.INTEGER,
     },
     {
       sequelize,
-      modelName: "User",
-      tableName: "users",
+      modelName: "Phone",
+      tableName: "phones",
       createdAt: "created_at",
       updatedAt: "updated_at",
     }
   );
-  return User;
+  return Phone;
 };
