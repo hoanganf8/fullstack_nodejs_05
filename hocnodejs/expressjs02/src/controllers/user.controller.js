637daff3b65d4d07e0d8752096ed596a841b5d53
Hoàng An F8 <hoangan@fullstack.edu.vn>
2024-07-30 12:35:33 +0700

    Update code

--- hocnodejs/expressjs02/src/controllers/user.controller.js
+++ hocnodejs/expressjs02/src/controllers/user.controller.js
@@ -1,7 +1,23 @@
-const { User, Phone } = require("../models/index");
+const { User, Phone, Post } = require("../models/index");
 module.exports = {
   index: async (req, res) => {
-    const users = await User.findAll();
+    const users = await User.findAll({
+      include: [
+        {
+          model: Post,
+          as: "posts",
+        },
+        {
+          model: Phone,
+          as: "phone",
+        },
+      ],
+    });
+    // for (let i = 0; i < users.length; i++) {
+    //   const user = users[i];
+    //   const posts = await user.getPosts();
+    //   user.setDataValue("posts", posts);
+    // }
     res.json({ users });
   },
   find: async (req, res) => {
@@ -23,5 +39,39 @@ module.exports = {
     }
     const user = await phoneInstance.getUser();
     res.json({ phone, user });
+  },
+  create: async (req, res) => {
+    const user = await User.create({
+      fullname: req.body.fullname,
+      email: req.body.email,
+      password: req.body.password,
+      status: req.body.status,
+    });
+    if (user) {
+      await user.createPhone({
+        value: req.body.phone,
+      });
+    }
+    return res.json({ user });
+  },
+  getPosts: async (req, res) => {
+    const { id } = req.params;
+    const user = await User.findByPk(id);
+    const posts = await user.getPosts();
+    res.json({ user, posts });
+  },
+  findByPost: async (req, res) => {
+    const { id } = req.params;
+    const post = await Post.findByPk(id);
+    const user = await post.getUser();
+    res.json({ post, user });
+  },
+  createPost: async (req, res) => {
+    const { userId } = req.params;
+    const user = await User.findByPk(userId);
+    const post = await user.createPost({
+      title: "Post 7",
+    });
+    return res.json({ user, post });
   },
 };
--- hocnodejs/expressjs02/src/models/post.js
+++ hocnodejs/expressjs02/src/models/post.js
@@ -1,43 +1,35 @@
 "use strict";
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
+  class Post extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
-      User.hasOne(models.Phone, {
+      Post.belongsTo(models.User, {
         foreignKey: "user_id",
-        as: "phone",
-      });
-      User.hasMany(models.Post, {
-        foreignKey: "user_id",
-        as: "posts",
+        as: "user",
       });
     }
   }
-  User.init(
+  Post.init(
     {
       id: {
         type: DataTypes.INTEGER,
         primaryKey: true,
         autoIncrement: true,
       },
-      fullname: DataTypes.STRING,
-      email: DataTypes.STRING,
-      password: DataTypes.STRING,
-      status: DataTypes.BOOLEAN,
+      title: DataTypes.STRING,
     },
     {
       sequelize,
-      modelName: "User",
-      tableName: "users",
+      modelName: "Post",
+      tableName: "posts",
       createdAt: "created_at",
       updatedAt: "updated_at",
     }
   );
-  return User;
+  return Post;
 };
